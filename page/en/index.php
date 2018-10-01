9222621009e443f50d6aa74363ff801e378b31b5
unknown <abdxax@gmail.com>
2018-10-01 20:39:31 +0300

    first up

---
+++ page/en/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<link rel="stylesheet" type="text/css" href="../../css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../../css/style.css">
+</head>
+<body>
+
+<header>
+
+</header>
+
+<section>
+	<div class="conatiner">
+		<div class="col-4">
+			<div class="card border-light">
+				<div class="card-heading ">
+					ddd
+				</div>
+			</div>
+		</div>
+		<div class="col-4">
+			<div class="card border-light">
+				<div class="card-heading ">
+					ddd
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<script src="../../js/bootstrap.js"></script>
+<script src="../../js/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+</body>
+</html>
